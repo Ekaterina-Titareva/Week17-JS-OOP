faa924ebd1703330a2cf47302076a456243a7053
Ekaterina-Titareva <ekaterina.titareva.ca@gmail.com>
2023-12-26 23:08:01 +0300

    added the part of the first task

--- week17/assets/script/week17.js
+++ week17/assets/script/week17.js
@@ -127,114 +127,189 @@ console.log(student2.increaseGrade());
 //Создайте на его основе объект book и выведите его свойства в консоль.
 
 //Ваш код
-
+function Book(title, author, year){
+	this.title = title;
+	this.author = author;
+	this.year = year;
+}
+const book = new Book();
+console.log(book);
 //Задание 12
 //Добавьте в класс Book метод getTitleAndAuthor, который будет возвращать строку в формате "Название книги - Автор". 
 //Создайте объект book2 на основе класса Book и выведите его название и автора в консоль с помощью метода getTitleAndAuthor.
 
 //Ваш код
-
+Book.prototype.getTitleAndAuthor = function(){
+	return `${this.title} - ${this.author}`
+}
+const book2 = new Book();
+console.log(book2.getTitleAndAuthor());
 //Задание 13
 //Создайте класс BankAccount. Класс должен содержать свойства accountNumber и balance. 
 //Создайте на его основе объект account и выведите его свойства в консоль.
 
 //Ваш код
-
+function BankAccount(accountNumber, balance){
+	this.accountNumber = accountNumber;
+	this.balance = balance;
+}
+const account = new BankAccount();
+console.log(account);
 //Задание 14
 //Добавьте в класс BankAccount метод deposit, который будет увеличивать баланс на заданную сумму. 
 //Создайте объект account2 на основе класса BankAccount, пополните его баланс с помощью метода deposit и выведите новый баланс в консоль.
 
 //Ваш код
+BankAccount.prototype.deposit = function(determineAmount){
+	return this.balance += determineAmount;
+}
+const account2 = new BankAccount(1, 100);
+console.log(account2.deposit(100));
 
 //Задание 15
 //В класс BankAccount добавлен метод withdraw, который уменьшает баланс на заданную сумму. 
 //Если запрошенная сумма превышает текущий баланс, выведите сообщение "Недостаточно средств". 
 //Создайте объект account3 на основе класса BankAccount, попробуйте снять с него сумму, превышающую баланс, и выведите соответствующее сообщение в консоль.
 
-class BankAccount {
-	constructor(balance) {
-		this.balance = balance;
-	}
-
-	withdraw(amount) {
+BankAccount.prototype.withdraw = function(amount) {
 		if (amount <= this.balance) {
 			//Уменьшите баланс на заданную сумму
+			return this.balance -= amount;
 		} else {
 			//Выводите в консоль сообщение
+			return console.log("Недостаточно средств");
 		}
 	}
-}
-//Создайте объект account3 на основе класса BankAccount
-//Попытайтесь снять сумму, превышающую баланс
 
+//Создайте объект account3 на основе класса BankAccount
+const account3 = new BankAccount(100);
+//Попытайтесь снять сумму, превышающую баланс
+account3.withdraw(200);
 //Задание 16
 //Создайте класс Animal. Класс должен содержать свойства name и sound. 
 //Создайте на его основе объект animal и выведите его свойства в консоль.
 
 //Ваш код
-
+function Animal(name, sound){
+	this.name = name;
+	this.sound = sound;
+}
+const animal = new Animal();
+console.log(animal);
 //Задание 17
 //Добавьте в класс Animal метод makeSound, который будет выводить в консоль звук животного. 
 //Создайте объект animal2 на основе класса Animal и вызовите метод makeSound.
 
 //Ваш код
-
+Animal.prototype.makeSound = function(){
+	return this.sound;
+}
+const animal2 = new Animal();
+console.log(animal2.makeSound());
 //Задание 18
 //Создайте класс Point. Класс должен содержать свойства x и y. 
 //Создайте на его основе объект point и выведите его свойства в консоль.
 
 //Ваш код
-
+function Point(x, y){
+	this.x = x;
+	this.y = y;
+}
+const point = new Point();
+console.log(point);
 //Задание 19
 //В классе Point добавлен метод getDistance, который будет возвращать расстояние от точки до начала координат (0,0). 
 //Создайте объект point2 на основе класса Point и выведите его расстояние до начала координат с помощью метода getDistance.
 
-class Point {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
 	// Метод getDistance возвращает расстояние от точки до начала координат (0,0)
-	getDistance() {
+Point.prototype.getDistance = function() {
 		return Math.sqrt(this.x ** 2 + this.y ** 2);
-	}
 }
-
 //Ваш код
-
+const point2 = new Point();
+console.log(point2.getDistance());
 //Задание 20
 //Создайте класс Calculator. Класс должен содержать методы add, subtract, multiply и divide, которые будут выполнять соответствующие 
 //математические операции над двумя числами и возвращать результат. Создайте объект calculator на основе класса Calculator, 
 //вызовите каждый из методов и выведите результаты в консоль.
 
 //Ваш код
+class Calculator {
+	add(a, b) {
+		return a + b;
+	}
+
+	subtract(a, b) {
+		return a - b;
+	}
+
+	multiply(a, b) {
+		return a * b;
+	}
+
+	divide(a, b) {
+		if (b !== 0) {
+		return a / b;
+		} else { 
+		console.log("Cannot divide by zero");
+		}
+	}
+}
+
+const calculator = new Calculator();
+
+console.log(calculator.add(5, 3));
+console.log(calculator.subtract(10, 4));
+console.log(calculator.multiply(2, 6));
+console.log(calculator.divide(10, 2));
 
 //Задание 21
 //Создайте класс Person. Класс должен содержать свойства name, age и city. 
 //Создайте на его основе объект person и выведите его свойства в консоль.
 
 //Ваш код
-
+function Person(name, age, city){
+this.name = name;
+this.age = age;
+this.city = city;
+}
+const person = new Person();
+console.log(person);
 //Задание 22
 //Добавьте в класс Person метод changeCity, который будет изменять значение свойства city на новое значение. 
 //Создайте объект person2 на основе класса Person, вызовите метод changeCity с аргументом "New York" и выведите новое значение свойства city в консоль.
 
 //Ваш код
-
+Person.prototype.changeCity = function(newCity){
+	return this.city = newCity;
+}
+const person2 = new Person();
+person2.changeCity("New York");
+console.log(person2.city);
 //Задание 23
 //Создайте класс Employee. Класс должен содержать свойства name, position и salary. 
 //Создайте на его основе объект employee и выведите его свойства в консоль.
 //Подсказка: Используйте методы класса, чтобы создать объект employee и вывести его свойства.
 
 //Ваш код
-
+function Employee(name, position, salary){
+	this.name = name;
+	this.position = position;
+	this.salary = salary;
+}
+const employee = new Employee();
+console.log(employee);
 //Задание 24
 //Добавьте в класс Employee метод calculateBonus, который будет принимать процент бонуса и увеличивать зарплату сотрудника на соответствующую сумму. 
 //Создайте объект employee2 на основе класса Employee и вызовите метод calculateBonus с процентом 10. Выведите новую зарплату сотрудника в консоль.
 //Подсказка: Используйте метод call или apply, чтобы вызвать метод calculateBonus для объекта employee2 и передать процент бонуса.
 
 //Ваш код
-
+Employee.prototype.calculateBonus = function(percent){
+	return this.salary * (1 + percent);
+}
+const employee2 = new Employee("Ann", "Developer", 5000)
+console.log(employee2.calculateBonus(10));
 //Задание 25
 //Создайте класс Product. Класс должен содержать свойства name, price и quantity. 
 //Создайте на его основе объект product и выведите его свойства в консоль.
